ed2b7a604d7291f015ef7d32ef995fe1bd50ea8c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-05-07 16:12:10 +0900

    wsモジュールでsocket.ioをサーバーとつなぐことができているような気がする

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var app = express();
 
-//サーバーの立ち上げ
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+//サーバーの立ち上げ
+var http = require('http');
+
+//指定したポートにきたリクエストを受け取れるようにする
+var server = http.createServer(app).listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+var io = require('socket.io').listen(server);
+
+/*
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({server:server});
+
+var connections = []; 
+wss.on('connection', function (ws) {
+  console.log('connect!!');
+  connections.push(ws);
+  ws.on('close', function () {
+    console.log('close');
+    connections = connections.filter(function (conn, i) {
+      return (conn === ws) ? false : true;
+    });
+  });
+  ws.on('message', function (message) {
+    console.log('message:', message);
+    connections.forEach(function (con, i) {
+      con.send(message);
+    });
+  });
+});
+*/
+
 app.get('/', function(req, res){
-  //res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -16,22 +45,17 @@ io.on('connection', function(socket){
   console.log('a user connected');
   //接続している、人達(socket)がサーバーにメッセーッジを送った時にcallbackされるイベントを登録
   //第一引数はイベント名
-  socket.on('grab', function(msg){
+  socket.on('message', function(msg){
     //受け取った人以外でつながっている人全員に送る場合(broadcastを使う)
     //socket.broadcast.emit('message', 'hello');
     //受け取った人含めて全員に送る場合
     //位第一引数のイベント名に対して送る
-    socket.broadcast.emit('grab', msg);
-    console.log('grab: ' + msg);
+    socket.broadcast.emit('message', msg);
+    console.log('message: ' + msg);
   });
 
   //サーバーとの接続が遮断されると呼ばれる
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-});
-
-//指定したポートにきたリクエストを受け取れるようにする
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
 });
