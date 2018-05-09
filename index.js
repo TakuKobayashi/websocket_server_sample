4202d882d40e92f89d3b9f33a0d4dfa85173d020
TakuKobayshi <>
2018-05-09 17:01:43 +0900

    I no longer use socket.io

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ var server = http.createServer(app).listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-//use socket.io server
-var io = require('socket.io').listen(server);
-
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({server:server});
 
@@ -43,25 +40,4 @@ app.get('/jquery/jquery.js', function(req, res) {
 });
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
-
-//サーバーと接続されると呼ばれる
-io.on('connection', function(socket){
-  console.log('a user connected');
-  //接続している、人達(socket)がサーバーにメッセーッジを送った時にcallbackされるイベントを登録
-  //第一引数はイベント名
-  socket.on('message', function(msg){
-    //受け取った人以外でつながっている人全員に送る場合(broadcastを使う)
-    //socket.broadcast.emit('message', 'hello');
-    //受け取った人含めて全員に送る場合
-    //位第一引数のイベント名に対して送る
-    //socket.broadcast.emit('message', msg);
-    io.emit('message', msg);
-    console.log('message: ' + msg);
-  });
-
-  //サーバーとの接続が遮断されると呼ばれる
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
 });
