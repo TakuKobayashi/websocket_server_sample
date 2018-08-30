9844ff878bdf60a7b70832e50ff525ca111b59e7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-31 02:52:50 +0900

    websocketのパスに対応したcontrollerを読み込んで実行できるようにした

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 var url = require('url');
-var port = process.env.PORT || 3000;
+
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var port = process.env.PORT || 3100;
 //var VideoEditor = require(__dirname + '/video_editor.js');
 
 //wake up http server
 var http = require('http');
-const WebSocket = require('ws');
+var WebSocket = require('ws');
+var glob = require("glob");
+
+var pathWebsockets = {}
+var CONTROLLER_ROOT_PATH = "/ws_controllers"
+var ws_controllers = glob.sync(__dirname + CONTROLLER_ROOT_PATH + "/**/*.js");
+for(var i = 0;i < ws_controllers.length;++i){
+  var controller_path = ws_controllers[i].match(new RegExp(CONTROLLER_ROOT_PATH + ".+")).toString();
+  var action_path = controller_path.replace(new RegExp("^" + CONTROLLER_ROOT_PATH), "").toString();
+  var key = action_path.replace(new RegExp(".js$"), "").toString();
+  pathWebsockets[key] = require(ws_controllers[i]);
+}
 
 //Enable to receive requests access to the specified port
 var server = http.createServer();
 
-var pathWebsockets = {}
-
 server.on('upgrade', function upgrade(request, socket, head) {
   var pathname = url.parse(request.url).pathname;
-  console.log(pathname);
-  if(!pathWebsockets[pathname] || !pathWebsockets[pathname].server){
-    var websocketServer = new WebSocket.Server({ noServer: true });
+  if(pathname[pathname.length - 1] == "/"){
+    pathname = pathname.substr(0, pathname.length - 2);
+  }
+  var controller = pathWebsockets[pathname];
+  if(!controller || !controller.server){
+    var websocketServer = new WebSocket.Server({noServer: true});
     var serverObject = {server: websocketServer, connections: []};
     websocketServer.on('connection', function connection(ws) {
+      ws.on('message', function (message) {
+        console.log('message:' + message);
+        if(controller && controller.message){
+          controller.message(websocketServer, ws, serverObject.connections, message);
+        }
+      });
       ws.on('close', function () {
         console.log('close');
+        if(controller && controller.close){
+          controller.close(websocketServer, ws, serverObject.connections);
+        }
         serverObject.connections = serverObject.connections.filter(function (conn, i) {
           return (conn === ws) ? false : true;
         });
--- ws_controllers/top.js
+++ ws_controllers/top.js
@@ -1,11 +1,11 @@
 module.exports = {
-  onmessage: function(wss, ws, connections, message){
+  message: function(wss, ws, connections, message){
     console.log('message:', message);
     connections.forEach(function (con, i) {
       con.send(message);
     });
   },
-  onclose: function(wss, ws, connections){
+  close: function(wss, ws, connections){
     console.log('close');
     connections = connections.filter(function (conn, i) {
       return (conn === ws) ? false : true;
