d50e22beb946a04f8f78d353359284689d4b8b22
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-23 19:49:22 +0900

    kintoneへ

--- ws_controllers/virtualykb.js
+++ ws_controllers/virtualykb.js
@@ -1,4 +1,18 @@
+const kintone = require('kintone-nodejs-sdk');
+const APIToken = 'rpqZfLqXvoPLq4ecTePoNhD74p64myQGSwK9iXiH'; // your API Token
+const myDomainName = 'taptappun.cybozu.com';
+const kintoneAuth = new kintone.Auth();
+kintoneAuth.setApiToken(APIToken);
+const kintoneConnection = new kintone.Connection(myDomainName, kintoneAuth);
+
 module.exports = {
-  message: function (wss, ws, connections, message) {},
+  message: function (wss, ws, connections, message) {
+    let kintoneRecord = new kintone.Record(kintoneConnection);
+    kintoneRecord.addRecord(6, {
+      message: {
+        value: message,
+      }
+    });
+  },
   close: function (wss, ws, connections) {}
 }
